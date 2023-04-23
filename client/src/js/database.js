83a4a1fae667437629de2f77b9809956ebe5238f
Thomas <taadamson33@gmail.com>
2023-04-23 18:10:17 -0400

    commit any sw changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const initdb = async () => {
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -11,6 +11,13 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, content: '' });
+  const result = await request;
+}
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
